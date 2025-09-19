0f8c3cd09877e2fda122d5cc52f9b9a27b8705b5
Ahmed Samir <mrabureem@gmail.com>
2025-09-19 16:10:35 +0300

    Finish Hero Slider Auto Change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
+const heroArr = ["graphic design", "web design", "online marketing & seo"];
+const heroTitle = document.querySelector(".hero-title");
+const bolits = document.querySelectorAll(".bolits span");
+let currentTitleIndex = 0;
+const currentTitle = heroArr[currentTitleIndex].split("");
+
+changeHeroText();
+
+setInterval(() => {
+  changeHeroText();
+}, 3000);
+
 const navIcon = document.querySelector(".nav-icon");
 const navLinks = document.querySelector(".nav-links");
 
-navIcon.addEventListener("click", ()=>{
-  navLinks.classList.toggle("active")
-})
+navIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+function removeActive() {
+  bolits.forEach((bolt) => {
+    bolt.classList.remove("active");
+  });
+}
+
+function changeHeroText() {
+  heroTitle.textContent = "";
+  heroTitle.textContent = heroArr[currentTitleIndex];
+  removeActive();
+  bolits[currentTitleIndex].classList.add("active");
+  currentTitleIndex++;
+  if (currentTitleIndex == heroArr.length) {
+    currentTitleIndex = 0;
+  }
+}
